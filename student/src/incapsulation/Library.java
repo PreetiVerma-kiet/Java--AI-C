53b66003644fb1b25c241f688931b6de272f97c7
Preeti Verma <preeti.verma@kiet.edu>
2025-09-02 11:20:56 +0530

    02/09/25: Incapsulation

---
+++ student/src/incapsulation/Library.java
@@ -0,0 +1,30 @@
+package incapsulation;
+
+public class Library {
+	private int rollno;
+	private int borrowed_books;
+	final private int MAX_BORROW=5;
+	
+	public void borrowBook() {
+		if(borrowed_books<MAX_BORROW) {
+			borrowed_books++;
+			System.out.println("The book has borrowed and total borrowed books are"+borrowed_books);
+		}
+		else {
+			System.out.println("Can not borrow the book more than 5");
+		}
+	}
+	public void returnBook() {
+		if(borrowed_books>0) {
+			borrowed_books--;
+			System.out.println("The book has been returned and total borrowd books are"+borrowed_books);
+		}
+		else {
+			System.out.println(" No book available to return");
+		}
+	}
+	public int getborrowed_books() {
+		return borrowed_books;
+	}
+	
+}
